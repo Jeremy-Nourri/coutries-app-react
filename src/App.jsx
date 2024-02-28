599d7048f6890d6afe51a1142c5cc6c46e838eb1
Jeremy-Nourri <nourri.jer@gmail.com>
2024-02-28 12:01:56 +0100

    feat: display message if no country found

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,66 @@
-import axios from 'axios';
-import { useEffect, useState, useRef } from 'react';
-import CountryList from './components/CountryList';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState, useRef } from "react";
+import CountryList from "./components/CountryList";
+import "./App.css";
 
 function App() {
   const [countriesList, setCountriesList] = useState([]);
   const [filteredCountries, setFilteredCountries] = useState([]);
+  const [isSubmitSearch, setIsSubmitSearch] = useState(false);
 
   const inputSearch = useRef();
 
   useEffect(() => {
-    axios.get('https://restcountries.com/v3.1/all?fields=id,translations,flags,region,population,capital')
-      .then(response => {
+    axios
+      .get(
+        "https://restcountries.com/v3.1/all?fields=id,translations,flags,region,population,capital"
+      )
+      .then((response) => {
         setCountriesList(response.data);
       })
-      .catch(error => console.error(error));
+      .catch((error) => console.error(error));
   }, []);
 
   const handleSubmitSearch = (e) => {
     e.preventDefault();
     const searchWordCleaned = inputSearch.current.value.toLowerCase().trim();
-    const countrySearched = countriesList.filter(country => country.translations.fra.common.toLowerCase() === searchWordCleaned);
+    const countrySearched = countriesList.filter(
+      (country) =>
+        country.translations.fra.common.toLowerCase() === searchWordCleaned
+    );
     setFilteredCountries(countrySearched);
+    setIsSubmitSearch(true);
   };
 
   const resetTheSearch = () => {
     setFilteredCountries([]);
-  }
+    setIsSubmitSearch(false);
+  };
 
   return (
     <>
       <header>
-
         <form onSubmit={handleSubmitSearch}>
           <label htmlFor="search">Rechercher un pays</label>
-          <input type="search" ref={inputSearch} id="search"  />
+          <input type="search" ref={inputSearch} id="search" />
         </form>
       </header>
       <main>
-        {filteredCountries.length === 0 ? (
+        {!isSubmitSearch ? (
           <CountryList countries={countriesList} />
-        ) : (
+        ) : filteredCountries.length > 0 ? (
           <>
             <CountryList countries={filteredCountries} />
-            <button onClick={resetTheSearch}>Revenir à la liste compléte</button>
+            <button onClick={resetTheSearch}>
+              Revenir à la liste compléte
+            </button>
+          </>
+        ) : (
+          <>
+            <p>Aucun pays ne correspond à votre recherche</p>
+            <button onClick={resetTheSearch}>
+              Revenir à la liste compléte
+            </button>
           </>
         )}
       </main>
